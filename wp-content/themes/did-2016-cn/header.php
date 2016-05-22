841e3451d6863051109ecbef93f94db2ccc2d189
Yaowen Zhang <yaowen.zhang@live.com>
2016-05-22 23:03:15 +0800

    update header

--- wp-content/themes/did-2016-cn/header.php
+++ wp-content/themes/did-2016-cn/header.php
@@ -2,7 +2,8 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui" />
+	<meta name="format-detection" content="telephone=no"/>
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<!--[if IE]>
 	<link rel="stylesheet" href="http://server-dev/did-v2/www/wp-content/themes/did-2016-cn/style-desktop.css" type="text/css" media="all" />
