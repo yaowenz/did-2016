021fc746f45fa237d439964ba5c4024625ff0763
Yaowen Zhang <yaowen.zhang@live.com>
2017-12-17 10:19:56 +0800

    upgrade to 4.9.1

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -1,49 +1,48 @@
 <?php
 /**
- * WordPress基础配置文件。
+ * The base configuration for WordPress
  *
- * 这个文件被安装程序用于自动生成wp-config.php配置文件，
- * 您可以不使用网站，您需要手动复制这个文件，
- * 并重命名为“wp-config.php”，然后填入相关信息。
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
  *
- * 本文件包含以下配置选项：
+ * This file contains the following configurations:
  *
- * * MySQL设置
- * * 密钥
- * * 数据库表名前缀
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
  * * ABSPATH
  *
- * @link https://codex.wordpress.org/zh-cn:%E7%BC%96%E8%BE%91_wp-config.php
+ * @link https://codex.wordpress.org/Editing_wp-config.php
  *
  * @package WordPress
  */
 
-// ** MySQL 设置 - 具体信息来自您正在使用的主机 ** //
-/** WordPress数据库的名称 */
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
 define('DB_NAME', 'database_name_here');
 
-/** MySQL数据库用户名 */
+/** MySQL database username */
 define('DB_USER', 'username_here');
 
-/** MySQL数据库密码 */
+/** MySQL database password */
 define('DB_PASSWORD', 'password_here');
 
-/** MySQL主机 */
+/** MySQL hostname */
 define('DB_HOST', 'localhost');
 
-/** 创建数据表时默认的文字编码 */
+/** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
 
-/** 数据库整理类型。如不确定请勿更改 */
+/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
 /**#@+
- * 身份认证密钥与盐。
+ * Authentication Unique Keys and Salts.
  *
- * 修改为任意独一无二的字串！
- * 或者直接访问{@link https://api.wordpress.org/secret-key/1.1/salt/
- * WordPress.org密钥生成服务}
- * 任何修改都会导致所有cookies失效，所有用户将必须重新登录。
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
@@ -59,38 +58,32 @@ define('NONCE_SALT',       'put your unique phrase here');
 /**#@-*/
 
 /**
- * WordPress数据表前缀。
+ * WordPress Database Table prefix.
  *
- * 如果您有在同一数据库内安装多个WordPress的需求，请为每个WordPress设置
- * 不同的数据表前缀。前缀名只能为数字、字母加下划线。
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
  */
 $table_prefix  = 'wp_';
 
 /**
- * 开发者专用：WordPress调试模式。
+ * For developers: WordPress debugging mode.
  *
- * 将这个值改为true，WordPress将显示所有用于开发的提示。
- * 强烈建议插件开发者在开发环境中启用WP_DEBUG。
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
  *
- * 要获取其他能用于调试的信息，请访问Codex。
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
 
-/**
- * zh_CN本地化设置：启用ICP备案号显示
- *
- * 可在设置→常规中修改。
- * 如需禁用，请移除或注释掉本行。
- */
-define('WP_ZH_CN_ICP_NUM', true);
+/* That's all, stop editing! Happy blogging. */
 
-/* 好了！请不要再继续编辑。请保存本文件。使用愉快！ */
-
-/** WordPress目录的绝对路径。 */
+/** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
 
-/** 设置WordPress变量和包含文件。 */
+/** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
