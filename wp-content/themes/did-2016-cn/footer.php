1431f2dce26fa5b87a772785a107f10f4fbe308f
Willis Zhang <yaowen.zhang@qq.com>
2019-08-13 12:43:50 +0800

    update footer link

--- wp-content/themes/did-2016-cn/footer.php
+++ wp-content/themes/did-2016-cn/footer.php
@@ -1,5 +1,5 @@
 <div class="content copyright">
-	<p>Copyright黑暗中对话（中国）版权所有 &nbsp;&nbsp;|&nbsp;&nbsp;沪ICP备12015459&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo site_url('contact')?>">联系我们</a></p>
+	<p>Copyright黑暗中对话（中国）版权所有 &nbsp;&nbsp;|&nbsp;&nbsp;<a href="http://www.beian.miit.gov.cn/">沪ICP备12015459</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?php echo site_url('contact')?>">联系我们</a></p>
 	<p>Dialogue in the Dark、Dialogue in Silence、Dialogue with time 是DSE的注册商标，黑暗中对话、无声中对话、时间对话概念及相关商标版权属于德国对话社会企业。</p>
 	<p style="margin:10px 0px"><a href="http://www.dialogue-se.com/"><img src="<?php echo get_template_directory_uri()?>/images/didse.png" width="100" /></a></p>
 </div>
