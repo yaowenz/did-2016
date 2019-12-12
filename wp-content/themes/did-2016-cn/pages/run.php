515fe341f311dae4d8c00c1c517c6370e599d7c6
Yaowen Zhang <yaowen.zhang@qq.com>
2019-12-12 11:10:29 +0800

    update text

--- wp-content/themes/did-2016-cn/pages/run.php
+++ wp-content/themes/did-2016-cn/pages/run.php
@@ -7,9 +7,7 @@
 				<img src="<?php echo get_template_directory_uri()?>/images/pages/run-1.jpg" />
 				<p style="margin-top:10px">本项目适合所有爱好运动，愿意挑战自我的团队。在三小时的活动中，团队成员将经历“暗中奔跑”等素质拓展活动，每个人都需要激活全新的资源和潜能以完成目标，与团队一起向前奔跑。
 				<p>“暗中奔跑”还能培养团队成员的同理心，鼓励大家接纳和欢迎“不同”，增强团队凝聚力。发掘自身和他人的潜能，更深刻地理解“平等”、“尊重”、“共融”理念。</p>
-				<p style="margin:5px"><span style="color:#aaa">· 人数：</span>无严格限制，40人以上需提前说明</p>
-				<p style="margin:5px"><span style="color:#aaa">· 时长：</span>3小时</p>
-				<p style="margin:5px"><span style="color:#aaa">· 价格：</span>¥400/人</p>
+				<p style="margin:5px"><span style="color:#aaa">· 人数：</span>无严格限制，40人以上需提前说明</p>				
 				<p style="margin:5px;color:#aaa">*为了保证团建效果，需至少提前一周联系黑暗中对话上海体验馆预定（户外素拓需至少提前两周）；</p>
 				<p style="margin:5px;color:#aaa">*活动当天请提前15分钟到场。</p>
 				<p style="margin:5px;color:#aaa">*更多详情请关注黑暗中对话微信公众号。</p>
